251a505cf194e56bd47e9c5c43f9501c811d1dc8
DESKTOP-34U2QPP\eslam <eslamadel3347@gmail.com>
2022-11-03 19:30:16 +0200

    PepareProject

---
+++ src/Services/Catalog/Catalog.api/Catalog.Api/Entites/Products.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Entites
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]  //indentify that is an object id in mongo
+        public int Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
